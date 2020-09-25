94c889f77e4fcdcbfe055690ccc193b662194dbb
Matt Pruitt <guitsaru@gmail.com>
2020-09-25 22:13:34 +0700

    Configure release

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -35,7 +35,7 @@ config :rest_client, RestClientWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :rest_client, RestClientWeb.Endpoint, server: true
+config :rest_client, RestClientWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
