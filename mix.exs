4ee7aab6b7ddf6d82936a49e9e9d2904c6fc828e
Matt Pruitt <guitsaru@gmail.com>
2020-09-27 13:57:45 +0700

    Run coveralls in test

--- mix.exs
+++ mix.exs
@@ -18,7 +18,8 @@ defmodule RestClient.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.github": :test
       ]
     ]
   end
